5a24db5b0ae4d6714c9e2ab0b9b11975c4f62be1
vinayait <vinaypandey.ait@gmail.com>
2017-09-02 21:14:48 -0500

    added a comment to test

--- continuousintegration/src/main/java/com/example/ContinuousintegrationApplication.java
+++ continuousintegration/src/main/java/com/example/ContinuousintegrationApplication.java
@@ -3,6 +3,8 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//testing after integration with gihub
+
 @SpringBootApplication
 public class ContinuousintegrationApplication {
 
